3aa2f8694c9f628ceba2088e1d7c48c033141a94
Rachel Allen <rqllen@gmail.com>
2024-04-29 20:08:04 -0500

    completed by RAA 29Apr24

--- booleans-and-conditionals/exercises/part-3.js
+++ booleans-and-conditionals/exercises/part-3.js
@@ -37,12 +37,8 @@ let commandOverride = true;
  
 /* 6) b) Code the following if/else check:
 If fuelLevel is above 20000 AND engineIndicatorLight is NOT red blinking OR commandOverride is true print "Cleared to launch!" Else print "Launch scrubbed!" */
-if ((fuelLevel > 20000) && (engineIndicatorLight !== "red blinking")) {
-    if (commandOverride === false) {
-        console.log("Launch scrubbed.");
-    } else {
-        console.log("Cleared to launch!");
-    }
+if (((fuelLevel > 20000) && (engineIndicatorLight !== "red blinking")) || (commandOverride === true)) {
+    console.log("Cleared to launch!");
 } else {
     console.log("Launch scrubbed.");
     }
