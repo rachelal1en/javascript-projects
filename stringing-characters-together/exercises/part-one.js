59016ebdbcd8e13d858cbf3afed94b29b3d9816c
Rachel Allen <rqllen@gmail.com>
2024-05-03 19:21:02 -0500

    completed by RAA 03May24

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -4,7 +4,13 @@ let num = 1001;
 console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
-
+console.log(num.toString().length);
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
-
+let numDecimal = 123.45;
+console.log(numDecimal.toString().replace(".","").length);
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+if (num.toString().includes(".")) {
+    console.log(num.toString().replace(".","").length);
+} else {
+    console.log(num.toString().length);
+}
