208d3688f24f5d6879a1acd01b9128c1388a46ba
Rachel Allen <rqllen@gmail.com>
2024-05-09 21:00:57 -0500

    raa 09may24

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -3,49 +3,54 @@ let protoArray2 = "A;C;M;E";
 let protoArray3 = "space delimited string";
 let protoArray4 = "Comma-spaces, might, require, typing, caution";
 
+strings = [protoArray1, protoArray2, protoArray3, protoArray4];
+
 //2) 
-function reverseCommas() {
+function reverseCommas(input) {
 	//TODO: 1. create and instantiate your variables.
-	let check;
+	let check = input;
 	let output;
-	let commasArray = [];
+	
 	//TODO: 2. write the code required for this step
-	for (let i=0; i>strings.length; ++i) {
-		if (strings[i].includes(",")) {
-			commasArray = strings[i].split(",");
-			commasArray.reverse();
-			console.log(commaSpace);
-		}
+	if (check.includes(",")){
+		output = check.split(",").reverse().join(",");
 	}
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
 	return output;
 }
 
 //3)
-function semiDash() {
-	let check;
+function semiDash(input) {
+	let check = input;
 	let output;
+	
 //TODO: write the code required for this step
-
+	if (check.includes(";")){
+	output = check.split(";").sort().join("-");
+	}
   
 	return output;
 }
 
 //4)
-function reverseSpaces() {
-	let check;
+function reverseSpaces(input) {
+	let check = input;
 	let output;
   //TODO: write the code required for this step
-
+  if (check.includes(" ")){
+	output = check.split(" ").sort().reverse().join(" ");
+	}
 	return output;
 }
 
 //5)
-function commaSpace() {
-	let check;
+function commaSpace(input) {
+	let check = input;
 	let output;
 	//TODO: write the code required for this step
-  
+	if (check.includes(", ")){
+		output = check.split(", ").reverse().join(",");
+	}
 	return output;
 }
 
