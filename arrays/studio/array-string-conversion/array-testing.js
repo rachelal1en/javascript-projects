49cbd560609af858b7ad8eaaa763e08e6a27a152
Rachel Allen <rqllen@gmail.com>
2024-05-09 20:09:58 -0500

    strings-modification RAA

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -3,15 +3,20 @@ let protoArray2 = "A;C;M;E";
 let protoArray3 = "space delimited string";
 let protoArray4 = "Comma-spaces, might, require, typing, caution";
 
-strings = [protoArray1, protoArray2, protoArray3, protoArray4];
-
 //2) 
 function reverseCommas() {
 	//TODO: 1. create and instantiate your variables.
 	let check;
 	let output;
+	let commasArray = [];
 	//TODO: 2. write the code required for this step
-
+	for (let i=0; i>strings.length; ++i) {
+		if (strings[i].includes(",")) {
+			commasArray = strings[i].split(",");
+			commasArray.reverse();
+			console.log(commaSpace);
+		}
+	}
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
 	return output;
 }
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -3,9 +3,25 @@ let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
-
+let removeLetters = 3;
+let shortStr = str.slice(removeLetters,str.length);
+let removeStr = str.slice(0,removeLetters);
+let newStr = shortStr + removeStr;
 //Use a template literal to print the original and modified string in a descriptive phrase.
-
+console.log(`${newStr} has nothing on ${str}`);
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
-
+removeLetters = input.question("How many letters do you want to move? ");
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+if (removeLetters>str.length){
+    removeLetters = 3;
+    let shortStr = str.slice(removeLetters,str.length);
+    let removeStr = str.slice(0,removeLetters);
+    let newStr = shortStr + removeStr;
+    console.log(`You selected more letter than exist in the word. You were defaulted to 3.
+    ${newStr} has nothing on ${str}`);
+} else {
+    let shortStr = str.slice(removeLetters,str.length);
+    let removeStr = str.slice(0,removeLetters);
+    let newStr = shortStr + removeStr;
+    console.log(`${newStr} has nothing on ${str}`);
+}
