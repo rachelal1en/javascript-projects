e99ed3479508e3e417867052e4d246b9e6fea248
Rachel Allen <rqllen@gmail.com>
2024-07-29 19:37:07 -0500

    RAA 29Jul24

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,10 +1,28 @@
-function init () {
+function init() {
     const missionAbort = document.getElementById("abortMission");
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+    });
+
+    missionAbort.addEventListener("mouseenter", function (event) {
+        event.target.style.backgroundColor = "red";
+    });
+    missionAbort.addEventListener("mouseout", function (event) {
+        event.target.style.backgroundColor = "";
+    });
+
+    missionAbort.addEventListener('click', event => {
+        if (confirm("Are you sure you want to abort the mission?")) {
+            paragraph.innerHTML = 'Mission aborted! Space shuttle returning home';
+        } else {
+            
+        }
+    })
     // Put your code for the exercises here.
-    
+
 }
 
 window.addEventListener("load", init);
